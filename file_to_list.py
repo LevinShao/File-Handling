f0929c26a8956b2086fa360da08997867cb56afc
Pixel Lemons <levin.shao@education.nsw.gov.au>
2025-04-10 22:51:25 +1000

    Finish

--- file_to_list.py
+++ file_to_list.py
@@ -1,8 +1,16 @@
 # TODO: Open the file "students.txt" in read mode
+with open("students.txt", "r") as file:
 
     # TODO: Read lines from the file and store them in a list
+    lines = file.readlines()
 
-#TODO: Remove extra spaces and newlines from each name. Use list comprehension to STRIP whitespace
+# TODO: Remove extra spaces and newlines from each name. Use list comprehension to STRIP whitespace
+cleaned_lines = [line.strip() for line in lines]
 
 # TODO: Print the list
-    # TODO: Print the cleaned-up list
+print("Original list:")
+print(lines)
+
+# TODO: Print the cleaned-up list
+print("\nCleaned list:")
+print(cleaned_lines)
